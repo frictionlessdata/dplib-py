7e056bc274b83b1bd5dbb549dae2eeb7803ff92d
roll <eskarev@gmail.com>
2023-11-28 17:15:11 +0000

    Rebase sql_schema on columns/constraints

--- dpspecs/plugins/sql/schema.py
+++ dpspecs/plugins/sql/schema.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 from typing import Any, Callable, List
 
 import sqlalchemy as sa
-from sqlalchemy.schema import Column, Constraint, Table
+from sqlalchemy.schema import Column, Constraint
+from typing_extensions import Self
 
 from ...model import Model
 from ...models import ForeignKey, ForeignKeyReference, Schema
@@ -12,7 +13,8 @@ from .field import SqlField
 
 
 class SqlSchema(Model, arbitrary_types_allowed=True):
-    table: Table
+    columns: List[Column[Any]] = []
+    constraints: List[Constraint] = []
 
     # Mappers
 
@@ -24,7 +26,7 @@ class SqlSchema(Model, arbitrary_types_allowed=True):
         table_name: str,
         dialect: str = settings.DEFAULT_DIALECT,
         with_metadata: bool = False,
-    ) -> Table:
+    ) -> Self:
         columns: List[Column[Any]] = []
         constraints: List[Constraint] = []
 
@@ -58,22 +60,25 @@ class SqlSchema(Model, arbitrary_types_allowed=True):
             constraint = sa.ForeignKeyConstraint(fk.fields, foreign_fields)
             constraints.append(constraint)
 
-        # Table
-        table = sa.Table(table_name, sa.MetaData(), *(columns + constraints))
-        return table
+        return SqlSchema(columns=columns, constraints=constraints)
 
-    def to_dp(self, *, with_metadata: bool = False) -> Schema:
+    def to_dp(
+        self,
+        *,
+        table_name: str,
+        with_metadata: bool = False,
+    ) -> Schema:
         schema = Schema()
 
         # Fields
-        for column in self.table.columns:
+        for column in self.columns:
             if with_metadata and column.name in settings.METADATA_IDENTIFIERS:
                 continue
             field = SqlField(column=column).to_dp()
             schema.fields.append(field)
 
         # Primary key
-        for constraint in self.table.constraints:
+        for constraint in self.constraints:
             if isinstance(constraint, sa.PrimaryKeyConstraint):
                 for column in constraint.columns:
                     if with_metadata and column.name in settings.METADATA_IDENTIFIERS:
@@ -81,14 +86,14 @@ class SqlSchema(Model, arbitrary_types_allowed=True):
                     schema.primaryKey.append(str(column.name))
 
         # Foreign keys
-        for constraint in self.table.constraints:
+        for constraint in self.constraints:
             if isinstance(constraint, sa.ForeignKeyConstraint):
                 resource = ""
                 own_fields: List[str] = []
                 foreign_fields: List[str] = []
                 for element in constraint.elements:
                     own_fields.append(str(element.parent.name))
-                    if element.column.table.name != self.table.name:
+                    if element.column.table.name != table_name:
                         resource = str(element.column.table.name)
                     foreign_fields.append(str(element.column.name))
                 ref = ForeignKeyReference(resource=resource, fields=foreign_fields)
