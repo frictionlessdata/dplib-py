f434f96f89194497a4b805cba71dc1cc7fcfa3c5
roll <eskarev@gmail.com>
2023-11-29 17:32:37 +0000

    Implemented ZenodoResource.to_dp

--- dpspecs/plugins/zenodo/resource.py
+++ dpspecs/plugins/zenodo/resource.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
+from ...helpers.resource import path_to_name
 from ...model import Model
+from ...models import Resource
 
 
 class ZenodoResource(Model):
@@ -10,3 +12,11 @@ class ZenodoResource(Model):
     key: str
     mimetype: str
     size: int
+
+    def to_dp(self) -> Resource:
+        resource = Resource(path=self.key, name=path_to_name(self.key))
+        resource.format = self.ext.lower()
+        resource.mimetype = self.mimetype
+        resource.bytes = self.size
+        resource.hash = self.checksum.replace("md5:", "")
+        return resource
