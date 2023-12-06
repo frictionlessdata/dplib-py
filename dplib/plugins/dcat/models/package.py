02a71bf33945d422ddb1fe07c7cb60b07ef81258
roll <eskarev@gmail.com>
2023-12-06 11:46:28 +0000

    Added mode dcat mappings

--- dplib/plugins/dcat/models/package.py
+++ dplib/plugins/dcat/models/package.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 from typing import List, Optional
 
-from rdflib import Graph, URIRef
-from rdflib.namespace import RDF, Namespace
+from rdflib import Graph
+from rdflib.namespace import FOAF, RDF, Namespace
 
 from dplib.model import Model
 
@@ -12,6 +12,7 @@ from . import parsers
 # References:
 # - https://www.w3.org/TR/vocab-dcat-2/
 # - https://joinup.ec.europa.eu/asset/dcat_application_profile
+# - https://github.com/ckan/ckanext-dcat/blob/master/ckanext/dcat/profiles.py
 
 
 class DcatPackage(Model):
@@ -46,13 +47,10 @@ class DcatPackage(Model):
         package = DcatPackage()
 
         # Identifier
-        try:
-            id = g.value(predicate=RDF.type, object=DCAT.Dataset)
-            if not isinstance(id, URIRef):
-                return
-            package.identifier = str(id)
-        except Exception:
+        id = parsers.id(g, predicate=RDF.type, object=DCAT.Dataset)
+        if not id:
             return
+        package.identifier = str(id)
 
         # Title
         title = parsers.string(g, subject=id, predicate=DCT.title)
@@ -69,9 +67,65 @@ class DcatPackage(Model):
         if version:
             package.version = version
 
+        # Landing page
+        landing_page = parsers.string(g, subject=id, predicate=DCAT.landingPage)
+        if landing_page:
+            package.landing_page = landing_page
+
+        # Issued
+        issued = parsers.string(g, subject=id, predicate=DCT.issued)
+        if issued:
+            package.issued = issued
+
+        # Modified
+        modified = parsers.string(g, subject=id, predicate=DCT.modified)
+        if modified:
+            package.modified = modified
+
+        # Accural periodicity
+        periodicity = parsers.string(g, subject=id, predicate=DCT.accrualPeriodicity)
+        if periodicity:
+            package.accural_periodicity = periodicity
+
+        # Provenance
+        provenance = parsers.string(g, subject=id, predicate=DCT.provenance)
+        if provenance:
+            package.provenance = provenance
+
+        # Keywords
+        keywords = parsers.strings(g, subject=id, predicate=DCAT.keyword)
+        if keywords:
+            package.keywords = keywords
+
+        # Languages
+        languages = parsers.strings(g, subject=id, predicate=DCT.language)
+        if languages:
+            package.languages = languages
+
+        # Themes
+        themes = parsers.strings(g, subject=id, predicate=DCAT.theme)
+        if themes:
+            package.themes = themes
+
+        # Alternate identifiers
+        identifiers = parsers.strings(g, subject=id, predicate=ADMS.identifier)
+        if identifiers:
+            package.alternate_identifiers = identifiers
+
+        # Conforms to
+        conforms_to = parsers.strings(g, subject=id, predicate=DCT.conformsTo)
+        if conforms_to:
+            package.comforms_to = conforms_to
+
+        # Documentation
+        documentation = parsers.strings(g, subject=id, predicate=FOAF.page)
+        if documentation:
+            package.documentation = documentation
+
         return package
 
 
-DCT = Namespace("http://purl.org/dc/terms/")
+ADMS = Namespace("http://www.w3.org/ns/adms#")
 DCAT = Namespace("http://www.w3.org/ns/dcat#")
+DCT = Namespace("http://purl.org/dc/terms/")
 OWL = Namespace("http://www.w3.org/2002/07/owl#")
--- dplib/plugins/dcat/models/parsers.py
+++ dplib/plugins/dcat/models/parsers.py
@@ -1,9 +1,18 @@
-from typing import Optional
+from typing import List, Optional
 
 from rdflib import Graph, Literal, URIRef
 
 
-def string(g: Graph, *, subject: URIRef, predicate: URIRef) -> Optional[str]:
+def id(g: Graph, *, predicate: URIRef, object: URIRef) -> Optional[URIRef]:
+    try:
+        id = g.value(predicate=predicate, object=object)
+        if isinstance(id, URIRef):
+            return id
+    except Exception:
+        pass
+
+
+def literal(g: Graph, *, subject: URIRef, predicate: URIRef) -> Optional[Literal]:
     default_lang = "en"
     items = list(g.objects(subject, predicate))
 
@@ -11,8 +20,29 @@ def string(g: Graph, *, subject: URIRef, predicate: URIRef) -> Optional[str]:
     for item in items:
         if isinstance(item, Literal):
             if item.language and item.language == default_lang:
-                return str(item)
+                return item
 
     # Otherwise, return the first item
-    if items:
-        return str(items[0])
+    for item in items:
+        if isinstance(item, Literal):
+            return item
+
+
+def string(g: Graph, *, subject: URIRef, predicate: URIRef) -> Optional[str]:
+    value = literal(g, subject=subject, predicate=predicate)
+    if value:
+        return str(value)
+
+
+def integer(g: Graph, *, subject: URIRef, predicate: URIRef) -> Optional[int]:
+    value = literal(g, subject=subject, predicate=predicate)
+    if value:
+        return int(value)
+
+
+def literals(g: Graph, *, subject: URIRef, predicate: URIRef) -> List[Literal]:
+    return [item for item in g.objects(subject, predicate) if isinstance(item, Literal)]
+
+
+def strings(g: Graph, *, subject: URIRef, predicate: URIRef) -> List[str]:
+    return [str(item) for item in literals(g, subject=subject, predicate=predicate)]
