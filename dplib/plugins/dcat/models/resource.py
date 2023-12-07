51e2134397b3f6e195c73af7ddeecd98835c77d0
roll <eskarev@gmail.com>
2023-12-07 15:15:52 +0000

    Sorted DcatResource props

--- dplib/plugins/dcat/models/resource.py
+++ dplib/plugins/dcat/models/resource.py
@@ -12,34 +12,37 @@ from .types import ISubject
 
 
 class DcatResource(Model):
-    title: Optional[str] = None
-    description: Optional[str] = None
     access_url: Optional[str] = None
+    byte_size: Optional[int] = None
+    conforms_to: List[str] = []
+    description: Optional[str] = None
     download_url: Optional[str] = None
     issued: Optional[str] = None
-    modified: Optional[str] = None
-    license: Optional[str] = None
-    byte_size: Optional[int] = None
-    media_type: Optional[str] = None
-
-    pages: List[str] = []
     languages: List[str] = []
-    conforms_to: List[str] = []
+    license: Optional[str] = None
+    media_type: Optional[str] = None
+    modified: Optional[str] = None
+    pages: List[str] = []
+    title: Optional[str] = None
 
     # Converters
 
     def to_graph(self, g: Graph, *, id: BNode):
-        # Title
-        if self.title:
-            dumpers.node(g, self.title, subject=id, predicate=ns.TITLE)
+        # Access URL
+        if self.access_url:
+            dumpers.node(g, self.access_url, subject=id, predicate=ns.ACCESS_URL)
+
+        # Byte size
+        if self.byte_size:
+            dumpers.node(g, self.byte_size, subject=id, predicate=ns.BYTE_SIZE)
+
+        # Conforms to
+        for conforms_to in self.conforms_to:
+            dumpers.node(g, conforms_to, subject=id, predicate=ns.COMFORMS_TO)
 
         # Description
         if self.description:
             dumpers.node(g, self.description, subject=id, predicate=ns.DESCRIPTION)
-
-        # Access URL
-        if self.access_url:
-            dumpers.node(g, self.access_url, subject=id, predicate=ns.ACCESS_URL)
 
         # Download URL
         if self.download_url:
@@ -49,33 +52,29 @@ class DcatResource(Model):
         if self.issued:
             dumpers.node(g, self.issued, subject=id, predicate=ns.ISSUED)
 
-        # Modified
-        if self.modified:
-            dumpers.node(g, self.modified, subject=id, predicate=ns.MODIFIED)
+        # Languages
+        for language in self.languages:
+            dumpers.node(g, language, subject=id, predicate=ns.LANGUAGE)
 
         # License
         if self.license:
             dumpers.node(g, self.license, subject=id, predicate=ns.LICENSE)
 
-        # Byte size
-        if self.byte_size:
-            dumpers.node(g, self.byte_size, subject=id, predicate=ns.BYTE_SIZE)
-
         # Media type
         if self.media_type:
             dumpers.node(g, self.media_type, subject=id, predicate=ns.MEDIA_TYPE)
+
+        # Modified
+        if self.modified:
+            dumpers.node(g, self.modified, subject=id, predicate=ns.MODIFIED)
 
         # Pages
         for page in self.pages:
             dumpers.node(g, page, subject=id, predicate=ns.PAGE)
 
-        # Languages
-        for language in self.languages:
-            dumpers.node(g, language, subject=id, predicate=ns.LANGUAGE)
-
-        # Conforms to
-        for conforms_to in self.conforms_to:
-            dumpers.node(g, conforms_to, subject=id, predicate=ns.COMFORMS_TO)
+        # Title
+        if self.title:
+            dumpers.node(g, self.title, subject=id, predicate=ns.TITLE)
 
         return g
 
@@ -83,20 +82,25 @@ class DcatResource(Model):
     def from_graph(cls, g: Graph, *, id: ISubject) -> DcatResource:
         resource = DcatResource()
 
-        # Name
-        title = loaders.string(g, subject=id, predicate=ns.TITLE)
-        if title:
-            resource.title = title
+        # Access URL
+        access_url = loaders.string(g, subject=id, predicate=ns.ACCESS_URL)
+        if access_url:
+            resource.access_url = access_url
+
+        # Byte size
+        byte_size = loaders.integer(g, subject=id, predicate=ns.BYTE_SIZE)
+        if byte_size:
+            resource.byte_size = byte_size
+
+        # Conforms to
+        conforms_to = loaders.strings(g, subject=id, predicate=ns.COMFORMS_TO)
+        if conforms_to:
+            resource.conforms_to = conforms_to
 
         # Description
         description = loaders.string(g, subject=id, predicate=ns.DESCRIPTION)
         if description:
             resource.description = description
-
-        # Access URL
-        access_url = loaders.string(g, subject=id, predicate=ns.ACCESS_URL)
-        if access_url:
-            resource.access_url = access_url
 
         # Download URL
         download_url = loaders.string(g, subject=id, predicate=ns.DOWNLOAD_URL)
@@ -108,39 +112,34 @@ class DcatResource(Model):
         if issued:
             resource.issued = issued
 
-        # Modified
-        modified = loaders.string(g, subject=id, predicate=ns.MODIFIED)
-        if modified:
-            resource.modified = modified
+        # Languages
+        languages = loaders.strings(g, subject=id, predicate=ns.LANGUAGE)
+        if languages:
+            resource.languages = languages
 
         # License
         license = loaders.string(g, subject=id, predicate=ns.LICENSE)
         if license:
             resource.license = license
 
-        # Byte size
-        byte_size = loaders.integer(g, subject=id, predicate=ns.BYTE_SIZE)
-        if byte_size:
-            resource.byte_size = byte_size
-
         # Media type
         media_type = loaders.string(g, subject=id, predicate=ns.MEDIA_TYPE)
         if media_type:
             resource.media_type = media_type
+
+        # Modified
+        modified = loaders.string(g, subject=id, predicate=ns.MODIFIED)
+        if modified:
+            resource.modified = modified
 
         # Pages
         pages = loaders.strings(g, subject=id, predicate=ns.PAGE)
         if pages:
             resource.pages = pages
 
-        # Languages
-        languages = loaders.strings(g, subject=id, predicate=ns.LANGUAGE)
-        if languages:
-            resource.languages = languages
-
-        # Conforms to
-        conforms_to = loaders.strings(g, subject=id, predicate=ns.COMFORMS_TO)
-        if conforms_to:
-            resource.conforms_to = conforms_to
+        # Title
+        title = loaders.string(g, subject=id, predicate=ns.TITLE)
+        if title:
+            resource.title = title
 
         return resource
