98639136198554184238ebb2b1f53e85c29fb556
roll <eskarev@gmail.com>
2023-12-11 15:18:41 +0000

    Support dereferencing in checks

--- dplib/actions/package/check.py
+++ dplib/actions/package/check.py
@@ -2,8 +2,24 @@ from typing import List, Union
 
 from ... import types
 from ...errors.metadata import MetadataError
+from ...helpers.data import read_data
+from ...helpers.path import infer_basepath
 from ..metadata.check import metadata_check
 
 
 def package_check(package: Union[str, types.IDict]) -> List[MetadataError]:
+    basepath = None
+    if isinstance(package, str):
+        basepath = infer_basepath(package)
+        package = read_data(package)
+
+    # Dereference resources[].dialect/schema
+    resources = package.get("resources", [])
+    if isinstance(resources, list):
+        for resource in resources:  # type: ignore
+            for name in ["dialect", "schema"]:
+                value = resource.get(name)  # type: ignore
+                if value and isinstance(value, str):
+                    resource[name] = read_data(value, basepath=basepath)
+
     return metadata_check(package, profile_name="data-package")
