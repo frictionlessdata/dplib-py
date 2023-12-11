39eb5889137395b316a98bb038f4932c29d8c92d
roll <eskarev@gmail.com>
2023-12-11 17:27:49 +0000

    Implemented dp schema check

--- dplib/plugins/cli/commands/schema/check.py
+++ dplib/plugins/cli/commands/schema/check.py
@@ -1,6 +1,13 @@
+from dplib.actions.schema.check import check_schema
+
+from ...helpers.check import print_check_results
+from ...options.path import path_arg
 from .main import program_schema
 
 
 @program_schema.command(name="check")
-def program_schema_check():
-    pass
+def program_schema_check(
+    path: str = path_arg,
+):
+    errors = check_schema(path)
+    print_check_results(path, errors)
---
+++ dplib/plugins/cli/helpers/check.py
@@ -0,0 +1,21 @@
+from typing import List
+
+from rich.console import Console
+from rich.table import Table
+
+from dplib.errors.metadata import MetadataError
+
+
+def print_check_results(path: str, errors: List[MetadataError]):
+    console = Console()
+    if not errors:
+        console.print(f"[bold green]Valid: {path}")
+        return
+    console.print(f"[bold red]Invalid: {path}", style="red")
+    console.rule("[bold white]Errors", style="white")
+    view = Table()
+    view.add_column("location")
+    view.add_column("message")
+    for error in errors:
+        view.add_row(error.object_path, error.message)
+    console.print(view)
