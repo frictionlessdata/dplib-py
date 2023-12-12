d20e159d082887717aef817af01c0779c20771f5
roll <eskarev@gmail.com>
2023-12-12 11:34:50 +0000

    Bootstrapped convert_metadata action

---
+++ dplib/actions/metadata/convert.py
@@ -0,0 +1,10 @@
+from typing import Optional
+
+from ...model import Model
+
+
+def convert_metadata(
+    path: str, *, source: Optional[str] = None, target: Optional[str] = None
+) -> Model:
+    """Convert metadata from one notation to another."""
+    pass
