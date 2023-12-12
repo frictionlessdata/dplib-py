f7799c42ffcaabb6c69a6ed2b30c44c08d62348a
roll <eskarev@gmail.com>
2023-12-12 13:04:33 +0000

    Implemented convert_metadata

--- dplib/actions/metadata/convert.py
+++ dplib/actions/metadata/convert.py
@@ -3,8 +3,6 @@ from __future__ import annotations
 from importlib import import_module
 from typing import Literal, Optional, Type, Union
 
-from ... import types
-from ...error import Error
 from ...model import Model
 from ...models import Package, Resource
 
@@ -24,22 +22,16 @@ def convert_metadata(
         module = import_module(f"dplib.plugins.{source}")
         Source: Type[Model] = getattr(module, f"{source.capitalize()}{type.capitalize()}")
     model = Source.from_path(path)
-    if hasattr(model, "to_dp"):
-        model = model.to_dp()
+    model = model.to_dp()
 
     # Target model
-    Target = Resource if type == "resource" else Package
     if target:
         module = import_module(f"dplib.plugins.{target}")
         Target: Type[Model] = getattr(module, f"{target.capitalize()}{type.capitalize()}")
+        model = Target.from_dp(model)
+
+    return model
 
 
 IType = Union[Literal["package"], Literal["resource"]]
 INotation = Union[Literal["ckan"], Literal["dcat"], Literal["github"], Literal["zenodo"]]
-
-
-#  def load_Resource(notation: INotation):
-#  if notation == "ckan":
-#  from dplib.plugins.ckan.models.resource import CkanResource
-
-#  return CkanResource
