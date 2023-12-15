c02254328a035b5056d2b18598d895ca73900439
roll <eskarev@gmail.com>
2023-12-15 21:50:59 +0000

    Fixed typer version

--- dplib/plugins/cli/commands/main.py
+++ dplib/plugins/cli/commands/main.py
@@ -13,7 +13,7 @@ program.add_typer(schema.program)
 
 @program.callback()
 def main(
-    debug: Debug = False,
+    debug: Debug = None,
 ):
     """
     Python implementation of the Data Package standard and
--- dplib/plugins/cli/options/system.py
+++ dplib/plugins/cli/options/system.py
@@ -1,7 +1,9 @@
+from typing import Optional
+
 from typer import Option
 from typing_extensions import Annotated
 
 Debug = Annotated[
-    bool,
-    Option(False, "--debug", "-d", help="Show debug information"),
+    Optional[bool],
+    Option("--debug", "-d", help="Show debug information"),
 ]
