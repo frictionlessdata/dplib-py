a454a68b3dcbb8392f6dd6cef40de6d1a1d70470
roll <eskarev@gmail.com>
2024-06-25 14:01:59 +0100

    Added `categories/categoriesOrdered`

--- dplib/models/field/datatypes/string.py
+++ dplib/models/field/datatypes/string.py
@@ -4,6 +4,7 @@ from typing import Literal, Optional, Union
 
 import pydantic
 
+from ..categories import ICategories
 from ..constraints import StringConstraints
 from .base import BaseField
 
@@ -22,3 +23,14 @@ class StringField(BaseField):
     type: Literal["string"] = "string"
     format: Optional[IStringFormat] = None
     constraints: StringConstraints = pydantic.Field(default_factory=StringConstraints)
+
+    categories: Optional[ICategories] = None
+    """
+    Property to restrict the field to a finite set of possible values
+    """
+
+    categoriesOrdered: bool = False
+    """
+    When categoriesOrdered is true, implementations SHOULD regard the order of
+    appearance of the values in the categories property as their natural order.
+    """
