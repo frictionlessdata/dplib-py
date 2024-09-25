b5162907977e9b223c5f31884b52ba728e40a2a5
roll <eskarev@gmail.com>
2024-09-25 10:23:46 +0100

    Skip permission tests on Windows

--- dplib/helpers/__spec__/test_file.py
+++ dplib/helpers/__spec__/test_file.py
@@ -1,10 +1,14 @@
 import os
+import platform
 import stat
 from pathlib import Path
+
+import pytest
 
 from dplib.helpers.file import write_file
 
 
+@pytest.mark.skipif(platform.system() == "Windows", reason="Unix only")
 def test_write_file_implicit_permissions_dp_002(tmpdir: Path):
     path = str(tmpdir / "test.txt")
     write_file(path, "Hello, World!")
@@ -12,6 +16,7 @@ def test_write_file_implicit_permissions_dp_002(tmpdir: Path):
     assert permissions == "0o600"
 
 
+@pytest.mark.skipif(platform.system() == "Windows", reason="Unix only")
 def test_write_file_explicit_permissions_dp_003(tmpdir: Path):
     path = str(tmpdir / "test.txt")
     write_file(path, "Hello, World!", permissions=0o644)
